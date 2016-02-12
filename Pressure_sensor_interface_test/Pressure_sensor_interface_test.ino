da69b27550e121d1375ac8b358b8f49694ded1b4
cancui <can.cui@hotmail.com>
2016-02-11 20:43:36 -0500

    Converts analog to Newtons force

---
+++ Pressure_sensor_interface_test/Pressure_sensor_interface_test.ino
@@ -0,0 +1,70 @@
+/* FSR testing sketch. 
+ 
+Connect one end of FSR to 5V, the other end to Analog 0.
+Then connect one end of a 10K resistor from Analog 0 to ground
+Connect LED from pin 11 through a resistor to ground 
+ 
+For more information see www.ladyada.net/learn/sensors/fsr.html */
+ 
+int fsrAnalogPin = 0; // FSR is connected to analog 0
+int LEDpin = 11;      // connect Red LED to pin 11 (PWM pin)
+int fsrReading;      // the analog reading from the FSR resistor divider
+int LEDbrightness;
+int fsrVoltage;     // the analog reading converted to voltage
+unsigned long fsrResistance;  // The voltage converted to resistance, can be very big so make "long"
+unsigned long fsrConductance; 
+long fsrForce;       // Finally, the resistance converted to force
+ 
+ 
+void setup(void) {
+  Serial.begin(9600);   // We'll send debugging information via the Serial monitor
+  pinMode(LEDpin, OUTPUT);
+}
+ 
+void loop(void) {
+  fsrReading = analogRead(fsrAnalogPin);
+  Serial.print("Analog reading = ");
+  Serial.println(fsrReading);
+ 
+  // we'll need to change the range from the analog reading (0-1023) down to the range
+  // used by analogWrite (0-255) with map!
+  LEDbrightness = map(fsrReading, 0, 1023, 0, 255);
+  // LED gets brighter the harder you press
+  analogWrite(LEDpin, LEDbrightness);
+
+  // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
+  fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
+  Serial.print("Voltage reading in mV = ");
+  Serial.println(fsrVoltage);  
+ 
+  if (fsrVoltage == 0) {
+    Serial.println("No pressure");  
+  } else {
+    // The voltage = Vcc * R / (R + FSR) where R = 10K and Vcc = 5V
+    // so FSR = ((Vcc - V) * R) / V        yay math!
+    fsrResistance = 5000 - fsrVoltage;     // fsrVoltage is in millivolts so 5V = 5000mV
+    fsrResistance *= 10000;                // 10K resistor
+    fsrResistance /= fsrVoltage;
+    Serial.print("FSR resistance in ohms = ");
+    Serial.println(fsrResistance);
+ 
+    fsrConductance = 1000000;           // we measure in micromhos so 
+    fsrConductance /= fsrResistance;
+    Serial.print("Conductance in microMhos: ");
+    Serial.println(fsrConductance);
+ 
+    // Use the two FSR guide graphs to approximate the force
+    if (fsrConductance <= 1000) {
+      fsrForce = fsrConductance / 80;
+      Serial.print("Force in Newtons: ");
+      Serial.println(fsrForce);      
+    } else {
+      fsrForce = fsrConductance - 1000;
+      fsrForce /= 30;
+      Serial.print("Force in Newtons: ");
+      Serial.println(fsrForce);           
+    }
+
+  }
+  Serial.println("--------------------");
+}
