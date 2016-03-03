61e821e122f3b6eb95dea86503297c9eb260c2b9
pbhattachan <presish.bhattachan@gmail.com>
2016-03-03 18:50:49 -0500

    Adjusting

--- Atlas_Touch/Atlas_Touch.cpp
+++ Atlas_Touch/Atlas_Touch.cpp
@@ -6,15 +6,17 @@ using namespace std;
 #include "Atlas_Touch.hpp"
 
 Atlas_Pressure_Sensor::Sensor_State::Sensor_State(){
-
+	RoC = 0;
+	isTouching = false;
 }
 
 Atlas_Pressure_Sensor::Sensor_State::Sensor_State(int nRoC, bool isTouchingNow){
-
+	RoC = nRoC;
+	isTouching = isTouchingNow;
 }
 
 Atlas_Pressure_Sensor::Sensor_State::~Sensor_State(){
-
+	
 }
 
 Atlas_Pressure_Sensor::Sensor_State::getRoC(){
@@ -26,21 +28,21 @@ Atlas_Pressure_Sensor::Sensor_State::isItTouching(){
 }
 
 Atlas_Pressure_Sensor::changeInResistance(int[] resistance){
-
+	
 }
 
 Atlas_Pressure_Sensor::changeInResistance(int[] resistance, Sensor_State state*){
-
+	
 }
 
 Atlas_Pressure_Sensor::onGound(int resistance){
-
+	
 } 
 
 Atlas_Pressure_Sensor::onGound(int resistance, Sensor_State* state){
-
+	
 }
 
 Atlas_Pressure_Sensor::getForce(unsigned int resistance, unsigned int voltage){
-
+	
 }
