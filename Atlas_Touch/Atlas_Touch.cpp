b915b470b64e3e562f50036f62f85160f81faa0d
pbhattachan <presish.bhattachan@gmail.com>
2016-03-03 18:22:25 -0500

    Implementing the CPP

---
+++ Atlas_Touch/Atlas_Touch.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+#include "Atlas_Touch.hpp"
+
+Atlas_Pressure_Sensor::Sensor_State::getRoC(){
+	return RoC;
+}
+Atlas_Pressure_Sensor::changeInResistance(int[] resistance){
+
+}
+
+Atlas_Pressure_Sensor::onGound(int resistance){
+
+} 
+
+Atlas_Pressure_Sensor::getForce(unsigned int resistance, unsigned int voltage){
+
+}
